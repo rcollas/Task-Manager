2618bb6b7d6003a99e8194d7da2006bd4f2a9391
rcollas <collas.robin@gmail.com>
2023-09-25 10:29:51 +0200

    addition: /tasks GET and POST method tests

--- backend/tests/test_tasks.py
+++ backend/tests/test_tasks.py
@@ -1,3 +1,7 @@
+import pytest
+from core.db import (get_db)
+
+
 def test_get_tasks(client):
     response = client.get('/tasks')
     assert response.status_code == 200
@@ -22,3 +26,17 @@ def test_post_task_missing_description(client):
     response = client.post('/tasks', json={'title': 'test'})
     assert response.status_code == 400
     assert response.json['message'] == 'No description provided.'
+
+
+def test_post_task(client, app):
+    title = 'hello'
+    description = 'world'
+    response = client.post('/tasks', json={'title': title, 'description': description})
+    assert response.status_code == 200
+
+    with app.app_context():
+        db = get_db()
+        task = db.execute("SELECT * FROM task ORDER BY id desc limit 1").fetchone()
+        assert task['title'] == title
+        assert task['description'] == description
+
